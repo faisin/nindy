98d0175e9b7b81103bedc2765809621dfb8cb1e5
faisin <83275517+faisin@users.noreply.github.com>
2026-08-21 07:57:36 +0800

    Update xray.sh

--- install/xray.sh
+++ install/xray.sh
@@ -222,11 +222,5 @@ echo -e " NGINX Config  : /etc/nginx/conf.d/xray.conf"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
 
-read -n 1 -s -r -p "Tekan apa saja untuk kembali ke menu..."
-
-if command -v menu &> /dev/null; then
-    menu
-else
-    exit 0
-fi
-
+sleep 3
+exit 0
